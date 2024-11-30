16db26a1931da3fa75066b5f466f70775f1163b4
Eduardo Renz <dudarenz@gmail.com>
2024-11-30 18:42:31 -0300

    created constants to save fiis

---
+++ constants.py
@@ -0,0 +1,15 @@
+MY_FIIS = [
+    "RZTR11",
+    "XPLG11",
+    "HGLG11",
+    "XPML11",
+    "VGHF11",
+    "VISC11",
+    "MXRF11",
+    "CPTS11",
+    "GARE11",
+    "PORD11",
+    "TEPP11",
+    "KNRI11",
+    "BTLG11",
+    "PVBI11"]
