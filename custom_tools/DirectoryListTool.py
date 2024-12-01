96a2c00538576bb9ef97cd9eae0a4b57190273f2
Eduardo Renz <dudarenz@gmail.com>
2024-12-01 14:16:26 -0300

    try some fixes

--- custom_tools/DirectoryListTool.py
+++ custom_tools/DirectoryListTool.py
@@ -2,11 +2,12 @@ from crewai_tools import BaseTool
 from typing import List
 import os
 
+
 class DirectoryListTool(BaseTool):
     name: str = "Directory List Tool"
     description: str = "Le o conteudo de todos os arquivos no diretório reports, quando o diretorio estiver vazio, uma mensagem de diretorio vazio"
 
-    def _run(self,*kargs) -> str:
+    def _run(self, *args, **kwargs) -> str:
         directory = os.listdir('reports')
         if len(directory) == 0:
             return 'Diretório vazio'
